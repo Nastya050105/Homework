f882ee76bbcf66fffa17cc32ba269f1ba8dc4f2c
Nastya050105 <116375649+Nastya050105@users.noreply.github.com>
2023-02-24 20:34:42 +0500

    Add files via upload

---
+++ 25.02/first.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// первая задача
+ int firstJob = 5;
+
+ // вторая задача
+ static int secondJob = 10;
+ void firstFile() {
+	 cout <<"Вывод статической глобальной переменной из первого файла "<< secondJob << endl;
+}
+
+ //третья задача
+ int thirdJob1 = 7;
+ int thirdJob2 = 9;
---
+++ 25.02/задача.cpp
@@ -0,0 +1,32 @@
+﻿#include "first.cpp"
+
+
+// функция для второй задачи
+void secondFile() {
+	cout <<"Вывод статической глобальной переменной из второго файла " << secondJob << endl;
+}
+
+
+// функция для третьей задачи
+void thirdJob() {
+	thirdJob1 = 1;
+	thirdJob2 = 2;
+	cout << "Третья задача " << thirdJob1 << " " << thirdJob2;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+
+	// первая задача
+	cout <<"Вывод глобальной переменной из второго файла " << firstJob << endl;;
+
+	// вторая задача
+	firstFile();
+	secondFile();
+
+	//третья задача
+	thirdJob();
+}
+
