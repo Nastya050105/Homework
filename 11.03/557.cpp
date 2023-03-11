f274725fecbd6011a688f848e6ffc3d843efcdee
Nastya050105 <116375649+Nastya050105@users.noreply.github.com>
2023-03-11 08:59:58 +0500

    Add files via upload

---
+++ 11.03/557.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include<vector>
+#include<stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int m, n, a, b, p;
+    scanf("%d %d %d %d %d", &m, &n, &a, &b, &p);
+    vector<vector<int > > r, res;
+    for (int i = 0; i < n; ++i) {
+        vector<int> t(n);
+        for (int j = 0; j < n; ++j) {
+            scanf("%d", &t[j]);
+            t[j] %= p;
+        }
+        r.push_back(t);
+    }
+    res = r;
+    for (int k = 0; k < m - 1; ++k) {
+        vector<vector<int>  > temp;
+        for (int i = 0; i < n; ++i) {
+            vector<int> t(n);
+            for (int j = 0; j < n; ++j) {
+                scanf("%d", &t[j]);
+            }
+            temp.push_back(t);
+        }
+        for (int i = 0; i < r.size(); ++i) {
+            for (int j = 0; j < r[i].size(); ++j) {
+                int s = 0;
+                for (int h = 0; h < n; ++h)
+                    s += r[i][h] * temp[h][j];
+                res[i][j] = s % p;
+            }
+        }
+        r = res;
+    }
+    printf("%d ", res[a - 1][b - 1]);
+    return 0;
+}
---
+++ 11.03/575.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    int numRows, numCols;
+    int numCollumns = 0;
+    int numWalls = 0;
+    cin >> numRows >> numCols;
+
+    vector<vector<int>> grid(numRows, vector<int>(numCols));
+
+    for (int i = 0; i < numRows; i++) {
+        for (int j = 0; j < numCols; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    
+    for (int i = 0; i < numRows - 1; i++) {
+        for (int j = 0; j < numCols - 1; j++) {
+            int currentColor = grid[i][j];
+            if (currentColor != grid[i + 1][j] || currentColor != grid[i][j + 1] || currentColor != grid[i + 1][j + 1]) {
+                numCollumns++;
+            }
+        }
+    }
+
+    
+    for (int i = 0; i < numRows - 1; i++) {
+        for (int j = 0; j < numCols; j++) {
+            if (grid[i][j] != grid[i + 1][j]) {
+                numWalls++;
+            }
+        }
+    }
+
+   
+    for (int j = 0; j < numCols - 1; j++) {
+        for (int i = 0; i < numRows; i++) {
+            if (grid[i][j] != grid[i][j + 1]) {
+                numWalls++;
+            }
+        }
+    }
+
+    
+    long double score = 0.2 * 0.2 * 3 * numCollumns + 0.2 * 0.8 * 3 * numWalls;
+    cout << score << endl;
+
+    return 0;
+}
